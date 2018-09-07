3f0abbbf0db83f50250431431a25f0eb6a32c454
Andy Granger <macguy.ag@icloud.com>
2018-09-07 10:09:28 -0500

    Update

--- dep-to-array.sh
+++ dep-to-array.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Read a column from a table in a MySQL database and store its contents in an array
+dbquery=($(mysql -uscriptuser -pr8vA4mcfqwE7KMQ6 -se "use jamfcluster; select serial_number from device_enrollment_program_devices;" 2>/dev/null | grep -v "mysql: [Warning] Using a password on the command line interface can be insecure."))
+{ [ "${#dbquery[@]}" -eq 0 ] || printf '%s\n' "${dbquery[@]}"; } > /home/serenity/dep_serials.csv
 
-dbquery=($(mysql -uscriptuser -pr8vA4mcfqwE7KMQ6 -e "use jamfcluster; select serial_number from device_enrollment_program_devices;"))
-
-echo ${dbquery[@]} > /home/serenity/dep_serials.csv
+# Read data from a column in a CSV file and store it in an array
+readarray -t serials < <(cut -d, -f1 /home/serenity/dep_serials.csv)
+printf '%s\n' "${serials[@]}"
