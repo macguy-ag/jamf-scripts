0672d9ebe44a87f5420e576231300407c4c0b2bc
Andy Granger <macguy.ag@icloud.com>
2018-09-07 09:55:09 -0500

    Added code to read the contents of the CSV back into an array.

--- dep-to-array.sh
+++ dep-to-array.sh
@@ -2,6 +2,6 @@
 
 # Read a column from a table in a MySQL database and store its contents in an array
 
-dbquery=($(mysql -u scriptuser -p r8vA4mcfqwE7KMQ6 -e "use jamfcluster; select serial_number from device_enrollment_program_devices;"))
+dbquery=($(mysql -uscriptuser -pr8vA4mcfqwE7KMQ6 -e "use jamfcluster; select serial_number from device_enrollment_program_devices;"))
 
 echo ${dbquery[@]} > /home/serenity/dep_serials.csv
